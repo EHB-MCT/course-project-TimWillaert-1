da27e8f94da5b63fb70ca0762bf12c4f2d242c13
Tim Willaert <tim.willaert@student.ehb.be>
2020-05-19 15:52:28 +0200

    Vue - Rails Locomotion & Audio

--- werkstukvue/src/App.vue
+++ werkstukvue/src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Vue Werkstuk"/> -->
     <VR/>
   </div>
 </template>
@@ -19,12 +17,10 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*{
+  margin: 0;
+  padding: 0;
 }
+
 </style>
--- werkstukvue/src/components/VR.vue
+++ werkstukvue/src/components/VR.vue
@@ -1,36 +1,78 @@
 <template>
     <div class="container">
-    <a-scene>
-    <a-entity environment="preset: forest;"></a-entity>
+    
+      <div class="card">
+        <button v-on:click="openVR">VR</button>
+      </div>
 
-      <!-- Player -->
-      <a-entity id="rig" movement-controls="controls: checkpoint" checkpoint-controls="mode: animate">
-        <a-entity camera
-                  position="0 1.6 0"
-                  look-controls="pointerLockEnabled: true">
-          <a-entity cursor
-                    position="0 0 -1"
-                    geometry="primitive: ring; radiusInner: 0.005; radiusOuter: 0.01;"
-                    material="color: #CCC; shader: flat;"></a-entity>
+      <a-scene animation-timeline="timeline: #timeline" v-if="startVR">
+        <a-assets>
+
+          <audio id="music" src="../assets/music.mp3"></audio>
+
+          <img id="header" src="../assets/header.jpg">
+          <img id="stars" src="../assets/stars.png">
+          <img id="flowers" src="../assets/flowers.jpg">
+          <img id="mandala" src="../assets/mandala.jpg">
+
+          <a-timeline id="timeline">
+            <a-timeline-animation select="#rig" name="1"></a-timeline-animation>
+            <a-timeline-animation select="#rig" name="2"></a-timeline-animation>
+          </a-timeline>
+
+        </a-assets>
+
+        <!-- Banner -->
+        <a-entity geometry="primitive: plane; height: 10; width: 15" position="0 5 -40" material="shader: standard; src: #header"></a-entity>
+
+        <!-- Player -->
+        <a-entity id="rig" position="0 1.6 0"
+        animation__1="property: position; to: 0 1.6 -1500; dur: 15000;"
+        animation__2="property: position; from: 0 1.6 -1500; to: 0 1.6 -4500; dur: 25000;">
+          <a-camera sound="src: #music; autoplay: true; positional: false;">
+            <a-cursor></a-cursor>
+          </a-camera>
         </a-entity>
-      </a-entity>
 
-      <!-- Checkpoints -->
-      <a-entity position="1 0 1">
-        <a-cylinder checkpoint radius="1" height="0.1" position="0 0 -10" color="#39BB82"></a-cylinder>
-      </a-entity>
+        <a-entity geometry="primitive: cylinder; height: 2000; radius: 20; open-ended: true"
+        position="0 0 -250" rotation="-90 0 0" material="shader: flat; side: double; color: #000000"></a-entity>
 
-    </a-scene>
+        <a-entity geometry="primitive: cylinder; height: 2000; radius: 20; open-ended: true"
+        position="0 0 -2550" rotation="-90 0 0" material="shader: flat; side: double; src: #mandala; repeat: 2 5"></a-entity>
+
+      </a-scene>
     </div>
 </template>
 
 <script>
 
 export default {
+ data(){
+   return{
+     startVR: false
+   }
+ },
+ methods: {
+   openVR(){
+     this.startVR = true;
+   }
+ }
 }
 
 </script>
 
 <style scoped>
+
+.container{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.card{
+  background-color: blue;
+  border-radius: 20px;
+}
 
 </style>
