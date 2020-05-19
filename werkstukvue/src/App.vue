59a7098cfc7c7a9b5f88ac2de11e2d8117bdadd9
Tim Willaert <tim.willaert@student.ehb.be>
2020-05-19 18:16:10 +0200

    Vue - Switched to Synthwave Concept

--- werkstukvue/src/App.vue
+++ werkstukvue/src/App.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import VR from './components/VR.vue'
 
 export default {
--- werkstukvue/src/components/VR.vue
+++ werkstukvue/src/components/VR.vue
@@ -1,45 +1,65 @@
 <template>
     <div class="container">
     
-      <div class="card">
-        <button v-on:click="openVR">VR</button>
+      <div class="card" v-if="!startVR">
+        <button v-on:click="openVR">Start experience</button>
       </div>
 
-      <a-scene animation-timeline="timeline: #timeline" v-if="startVR">
+      <a-scene v-if="startVR" background="color: #ECECEC">
+
+        <a-entity environment="preset: starry; dressingAmount: 500" scale="5 5 5"></a-entity>
+
         <a-assets>
-
+          <a-asset-item id="model" src="model.obj"></a-asset-item>
+          <a-asset-item id="material" src="material.mtl"></a-asset-item>
           <audio id="music" src="../assets/music.mp3"></audio>
+        </a-assets>
 
-          <img id="header" src="../assets/header.jpg">
-          <img id="stars" src="../assets/stars.png">
-          <img id="flowers" src="../assets/flowers.jpg">
-          <img id="mandala" src="../assets/mandala.jpg">
+
+        <a-entity obj-model="obj: #model; mtl: #material" scale="0.01 0.01 0.01">
+        </a-entity>
+
+        <a-entity id="rig" position="0.32 -0.7 0.3">
+          <a-camera sound="src: #music; autoplay: true; positional: false;">
+          </a-camera>
+        </a-entity>
+      <!--
+        <a-assets>
 
           <a-timeline id="timeline">
             <a-timeline-animation select="#rig" name="1"></a-timeline-animation>
-            <a-timeline-animation select="#rig" name="2"></a-timeline-animation>
           </a-timeline>
 
         </a-assets>
 
-        <!-- Banner -->
-        <a-entity geometry="primitive: plane; height: 10; width: 15" position="0 5 -40" material="shader: standard; src: #header"></a-entity>
 
-        <!-- Player -->
+        <a-entity geometry="primitive: plane; height: 10; width: 15" position="0 1.6 -150" material="shader: standard; src: #header; side: double"></a-entity>
+
+
         <a-entity id="rig" position="0 1.6 0"
-        animation__1="property: position; to: 0 1.6 -1500; dur: 15000;"
-        animation__2="property: position; from: 0 1.6 -1500; to: 0 1.6 -4500; dur: 25000;">
+        animation__1="property: position; to: 0 1.6 -1500; dur: 15000;">
           <a-camera sound="src: #music; autoplay: true; positional: false;">
-            <a-cursor></a-cursor>
           </a-camera>
         </a-entity>
 
-        <a-entity geometry="primitive: cylinder; height: 2000; radius: 20; open-ended: true"
-        position="0 0 -250" rotation="-90 0 0" material="shader: flat; side: double; color: #000000"></a-entity>
+        <a-entity rotation="-90 0 0">
+          <a-entity geometry="primitive: cylinder; height: 1000; radius: 20; open-ended: true"
+          position="0 0 0" rotation="0 0 0" material="shader: flat; side: double; src: #stars" 
+          animation="property: rotation; to: 0 -360 0; loop: true; dur: 30000"></a-entity>
+        </a-entity>
 
-        <a-entity geometry="primitive: cylinder; height: 2000; radius: 20; open-ended: true"
-        position="0 0 -2550" rotation="-90 0 0" material="shader: flat; side: double; src: #mandala; repeat: 2 5"></a-entity>
+        <a-entity geometry="primitive: plane; height: 100; width: 100;" position="0 0 -493"
+          material="shader: flat; src: #stars; side: double"></a-entity>
 
+        <a-entity rotation="-90 0 0" position="0 0 -730">
+          <a-entity geometry="primitive: cylinder; height: 450; radius: 20; open-ended: true"
+           rotation="0 0 0" material="shader: flat; side: double; src: #mandala" 
+          animation="property: rotation; to: 0 -360 0; loop: true; dur: 20000"></a-entity>
+        </a-entity>
+
+        <a-entity geometry="primitive: plane; height: 100; width: 100;" position="0 0 -943"
+          material="shader: flat; src: #mandala; side: double"></a-entity>
+          !-->
       </a-scene>
     </div>
 </template>
