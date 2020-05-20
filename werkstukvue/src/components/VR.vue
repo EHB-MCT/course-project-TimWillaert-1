94301e5c8735d765ac4e89f643e1ba78b0dc75eb
Tim Willaert <tim.willaert@student.ehb.be>
2020-05-20 16:08:16 +0200

    Vue - Split Modes

--- werkstukvue/src/components/VR.vue
+++ werkstukvue/src/components/VR.vue
@@ -2,71 +2,71 @@
     <div class="container">
     
       <div class="card" v-if="!startVR">
-        <button v-on:click="openVR">Start experience</button>
+        <select name="song" id="song" v-model="song">
+          <option value="takeonme">Take On Me</option>
+          <option value="playingwiththeboys">Playing with the boys</option>
+        </select>
+        <button v-on:click="openVR('driver')">Driver mode</button>
+        <button v-on:click="openVR('dj')">Diskjockey mode</button>
       </div>
 
-      <a-scene v-if="startVR" background="color: #ECECEC">
+      <a-scene v-if="startVR">
 
-        <a-entity environment="preset: starry; dressingAmount: 500" scale="5 5 5"></a-entity>
+        <a-entity environment="preset: starry;"></a-entity>
 
         <a-assets>
           <a-asset-item id="model" src="model.obj"></a-asset-item>
-          <a-asset-item id="material" src="material.mtl"></a-asset-item>
-          <audio id="music" src="../assets/music.mp3"></audio>
+          <a-asset-item id="material" src="model.mtl"></a-asset-item>
+          <a-asset-item id="djmodel" src="platform.obj"></a-asset-item>
+          <a-asset-item id="djmaterial" src="platform.mtl"></a-asset-item>
+
+          <img id="sun" src="../assets/sun.png">
+
+          <a-mixin id="beveled-square" geometry="primitive: cone; radiusTop: 0.15; radiusBottom: 0.19; height: 0.02; segmentsRadial: 4; segmentsHeight: 1" rotation="0 45 0"></a-mixin>
+          <a-mixin id="square" geometry="primitive: box; width: 0.18; height: 0.025; depth: 0.18;" position="0 0.02 0"></a-mixin>
+          <a-mixin id="blue" material="color: #1E2768;"></a-mixin>
+          <a-mixin id="darkgreen" material="color: #22FF90;"></a-mixin>
+          <a-mixin id="yellow" material="color: #FFF88E;"></a-mixin>
+          <a-mixin id="offset" position="0 0.01 0"></a-mixin>
+
+          <a-asset-item id="optimerBoldFont" src="https://rawgit.com/mrdoob/three.js/dev/examples/fonts/optimer_bold.typeface.json"></a-asset-item>
         </a-assets>
 
-        <a-entity animation="property: position; to: 0 0 -100; dur: 30000;">
-          <a-entity id="car" obj-model="obj: #model; mtl: #material" scale="0.01 0.01 0.01">
+        <a-entity position="0 0 1.314" animation="property: position; to: 0 0 -0.708; loop: true; easing: linear; dur: 500;">
+          <a-entity v-if="mode == 'driver'" id="car" obj-model="obj: #model; mtl: #material" scale="0.01 0.01 0.01">
+          </a-entity>
+          <a-entity v-if="mode == 'dj'" id="platform" obj-model="obj: #djmodel; mtl: #djmaterial" scale="0.009 0.009 0.009" position="0.440 0 -1.453">
           </a-entity>
 
-          <a-entity id="rig" position="0.32 -0.63 0.3">
-            <a-camera sound="src: #music; autoplay: true; positional: false; loop: true;">
+          <a-entity v-if="mode == 'driver'" id="rig" position="0.32 -0.63 0.3">
+            <a-camera look-controls="pointerLockEnabled: true">
             </a-camera>
-            <a-entity id="leftHand" sphere-collider="objects: #car" super-hands hand-controls="hand: left; handModelStyle: highPoly; color: #ffcccc"></a-entity>
-            <a-entity id="rightHand" sphere-collider="objects: #car" super-hands hand-controls="hand: right; handModelStyle: highPoly; color: #ffcccc"></a-entity>
+            <a-entity id="leftHand" hand-controls="hand: left; handModelStyle: highPoly; color: #ffcccc"></a-entity>
+            <a-entity id="rightHand" hand-controls="hand: right; handModelStyle: highPoly; color: #ffcccc"></a-entity>
           </a-entity>
+
+          <a-entity v-if="mode == 'dj'" id="rig" position="0 0 0.4">
+            <a-camera look-controls="pointerLockEnabled: true">
+            </a-camera>
+            <a-entity id="leftHand" hand-controls="hand: left; handModelStyle: highPoly; color: #ffcccc"></a-entity>
+            <a-entity id="rightHand" hand-controls="hand: right; handModelStyle: highPoly; color: #ffcccc"></a-entity>
+          </a-entity>
+
+          <a-entity id="kicks" geometry="primitive: box; width: 1; height: 3; depth: 0.2;" position="0 0 -0.5" rotation="10 180 0" material="shader: flat; color: #ff1493; opacity: 0.5; transparent: true;">
+            <a-entity text-geometry="value: Kicks; font: #optimerBoldFont; size: 0.2" rotation="0 180 0" position="0.35 1.25 -0.12" material="shader: flat; color: #0006b1;"></a-entity>
+          </a-entity>
+
+          <a-entity id="snares" geometry="primitive: box; width: 1; height: 3; depth: 0.2;" position="0 0 0.9" rotation="-10 180 0" material="shader: flat; color: #0006b1; opacity: 0.5; transparent: true;">
+
+          </a-entity>
+
+          <a-entity v-on:buttondown="clickBtn('kick')" id="button" ui-button="base: beveled-square, darkgreen; top: square, blue; pressed: yellow, offset" position="-0.188 1 0.446"></a-entity>
+          <a-entity v-on:buttondown="clickBtn('clap')" id="button" ui-button="base: beveled-square, darkgreen; top: square, yellow; pressed: blue, offset" position="-0.188 1 0.132"></a-entity>
         </a-entity>
         
         <a-entity light="type: ambient; color: #CCC; intensity: 0.1"></a-entity>
         <a-entity light="type: directional; color: #FDB813; intensity: 0.1;" position="0 0 1"></a-entity>
 
-      <!--
-        <a-assets>
-
-          <a-timeline id="timeline">
-            <a-timeline-animation select="#rig" name="1"></a-timeline-animation>
-          </a-timeline>
-
-        </a-assets>
-
-
-        <a-entity geometry="primitive: plane; height: 10; width: 15" position="0 1.6 -150" material="shader: standard; src: #header; side: double"></a-entity>
-
-
-        <a-entity id="rig" position="0 1.6 0"
-        animation__1="property: position; to: 0 1.6 -1500; dur: 15000;">
-          <a-camera sound="src: #music; autoplay: true; positional: false;">
-          </a-camera>
-        </a-entity>
-
-        <a-entity rotation="-90 0 0">
-          <a-entity geometry="primitive: cylinder; height: 1000; radius: 20; open-ended: true"
-          position="0 0 0" rotation="0 0 0" material="shader: flat; side: double; src: #stars" 
-          animation="property: rotation; to: 0 -360 0; loop: true; dur: 30000"></a-entity>
-        </a-entity>
-
-        <a-entity geometry="primitive: plane; height: 100; width: 100;" position="0 0 -493"
-          material="shader: flat; src: #stars; side: double"></a-entity>
-
-        <a-entity rotation="-90 0 0" position="0 0 -730">
-          <a-entity geometry="primitive: cylinder; height: 450; radius: 20; open-ended: true"
-           rotation="0 0 0" material="shader: flat; side: double; src: #mandala" 
-          animation="property: rotation; to: 0 -360 0; loop: true; dur: 20000"></a-entity>
-        </a-entity>
-
-        <a-entity geometry="primitive: plane; height: 100; width: 100;" position="0 0 -943"
-          material="shader: flat; src: #mandala; side: double"></a-entity>
-          !-->
       </a-scene>
     </div>
 </template>
@@ -77,11 +77,24 @@ export default {
  data(){
    return{
      startVR: false,
+     mode: '',
+     song: 'takeonme'
    }
  },
  methods: {
-   openVR(){
+   openVR(mode){
+     this.mode = mode;
      this.startVR = true;
+
+    setTimeout(()=>{
+      var audio = new Audio(require('../assets/'+this.song+'.mp3'));
+      audio.loop = true;
+      audio.play();
+    },5000);
+   },
+   clickBtn(instrument){
+     var audio = new Audio(require('../assets/'+instrument+'.wav'));
+     audio.play();
    }
  }
 }
@@ -98,8 +111,12 @@ export default {
 }
 
 .card{
-  background-color: blue;
   border-radius: 20px;
+}
+
+button{
+  padding: 10px;
+  margin: 10px;
 }
 
 </style>
